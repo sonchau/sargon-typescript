d15fad09a2c8d410cab842c992bac54d1c8c5f88
“Son <son.chau@geografia.com.au>
2020-08-09 08:49:33 +1000

    add header and footer

---
+++ components/Footer.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react'
+import styled from "styled-components"
+
+const FooterStyled = styled.footer`
+    font-size: 1.5em;
+    padding: 2rem;
+    text-align: center;
+    background: #101d2c;
+    color: #fff;
+`;
+
+const NavStyled = styled.nav`
+    list-style: none;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(15rem, 1fr));
+    grid-gap: 2rem;
+    align-items: center; 
+    a {
+        font-size: 1.4rem;
+        color: #fff;
+        text-decoration: none;
+        font-family: "Josefin Sans", sans-serif;
+        text-transform: uppercase;
+        text-align: center;
+        padding: 1.5rem;
+        display: block;
+        transition: all .2s;
+        &:hover,
+        &:active {
+            background-color: rgba(255, 255, 255, 0.05);
+            transform: translateY(-3px); 
+        }
+    }
+`;
+
+type Props = {
+  footerText: String
+}
+
+const Footer = ({footerText}: Props) => (
+  <FooterStyled>
+      <NavStyled>
+          <li><a target="_blank" rel="noopener noreferrer" href="https://github.com/sonchau/sargon-react-context">Repos</a></li>
+          <li><a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/sonnchau/">Linkedin</a></li>
+      </NavStyled>
+      <p className="copyright">&copy; {footerText}.</p>
+  </FooterStyled>
+)
+export default Footer
---
+++ components/Header.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react'
+import styled from "styled-components"
+
+const HeaderStyled = styled.header`
+  font-size: 3.5em;
+  padding: 3rem;
+  text-align: center;
+  background: #101d2c;
+  color: #fff;
+`;
+
+type Props = {
+  headerText: String
+}
+
+const Header = ({ headerText }: Props) => (
+  <HeaderStyled>
+    <p>{headerText}</p>
+  </HeaderStyled>
+)
+
+export default Header
