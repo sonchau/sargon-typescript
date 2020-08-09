4aaa45444e00b7f0d361745a4c132056bafc4415
“Son <son.chau@geografia.com.au>
2020-08-09 15:51:31 +1000

    remove debug

--- api/flickr.tsx
+++ api/flickr.tsx
@@ -1,15 +1,12 @@
-import { Photo } from '../interfaces'
 import fetchJsonp from 'fetch-jsonp';
 
 export const FLICKR_API: String =
   'https://api.flickr.com/services/feeds/photos_public.gne?format=json&tagmode=any';
 
-
-  
 export const fetchPhotos = async (tags: string) => {
   try {
     const endpoint = `${FLICKR_API}&tags=${tags}`;
-    console.log('endpoint', endpoint)
+    //console.log('endpoint', endpoint)
     const response = await fetchJsonp(endpoint, {
       jsonpCallback: 'jsoncallback',
       timeout: 3000
