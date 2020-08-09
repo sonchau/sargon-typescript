f14445d423230e65213e2a80cfede834dba1d896
“Son <son.chau@geografia.com.au>
2020-08-09 15:29:06 +1000

    add global style

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import Layout from '../components/Layout'
 import Main from '../components/Main'
 import { Photo } from '../interfaces'
 
-
 const IndexPage = () => {
   return (
     <Layout title="Sargon Coding Challenge - Typescript">
